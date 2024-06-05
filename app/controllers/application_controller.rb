bdd7e2f7bff9de5c25eea611ef193911ec942256
Hirocky23 <key.hiroki23@gmail.com>
2024-06-05 11:40:14 +0900

    修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,19 @@
+# app/controllers/application_controller.rb
 class ApplicationController < ActionController::Base
   before_action :basic_auth
 
   def index
-
+    respond_to do |format|
+      format.html # index.html.erbが表示されます
+      format.json { render json: { message: "Welcome to the Application" } }
+    end
   end
 
   private
 
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
-      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]  # 環境変数を読み込む記述に変更
+      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
     end
   end
 end
