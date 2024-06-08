60e51924819d57b183b4ad72d750bcbdcfb49e39
Hirocky23 <key.hiroki23@gmail.com>
2024-06-08 21:34:38 +0900

    新規登録とログインのリンク作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'furima/index'
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root 'furima#index'
+  root to: 'items#index'
+  resources :items
 end
