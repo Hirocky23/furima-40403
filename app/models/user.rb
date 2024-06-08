034cb4736f786612d4b8327c790badc223c62afe
Hirocky23 <key.hiroki23@gmail.com>
2024-06-08 22:01:18 +0900

    バリデーション追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,21 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-end
+
+         validates :nickname, presence: true
+         validates :last_name, presence: true
+         validates :first_name, presence: true
+         validates :last_name_kana, presence: true
+         validates :first_name_kana, presence: true
+         validates :birth_date, presence: true
+
+         validate :password_complexity
+
+         private
+       
+         def password_complexity
+           return if password.blank? || password =~ /(?=.*\d)(?=.*[a-zA-Z])/
+       
+           errors.add :password, 'は半角英数字混合で入力してください'
+         end
+       end
