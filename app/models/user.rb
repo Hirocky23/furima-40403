168671a34a80671dd7df9ac3ed4ef2b068bf236d
Hirocky23 <key.hiroki23@gmail.com>
2024-06-09 20:19:02 +0900

    モデル単体テスト実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,20 +4,20 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-         validates :nickname, presence: true
-         validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: 'は全角（漢字・ひらがな・カタカナ）で入力してください' }
-         validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: 'は全角（漢字・ひらがな・カタカナ）で入力してください' }
-         validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: 'は全角（カタカナ）で入力してください' }
-         validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: 'は全角（カタカナ）で入力してください' }
-         validates :birth_date, presence: true
+  validates :nickname, presence: true
+  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: 'は全角（漢字・ひらがな・カタカナ）で入力してください' }
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: 'は全角（漢字・ひらがな・カタカナ）で入力してください' }
+  validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: 'は全角（カタカナ）で入力してください' }
+  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: 'は全角（カタカナ）で入力してください' }
+  validates :birth_date, presence: true
 
-         validate :password_complexity
+  validate :password_complexity
 
-         private
-       
-         def password_complexity
-           return if password.blank? || password =~ /(?=.*\d)(?=.*[a-zA-Z])/
-       
-           errors.add :password, 'は半角英数字混合で入力してください'
-         end
-       end
+  private
+
+  def password_complexity
+    return if password.blank? || password =~ /(?=.*\d)(?=.*[a-zA-Z])/
+
+    errors.add :password, 'は半角英数字混合で入力してください'
+  end
+end
