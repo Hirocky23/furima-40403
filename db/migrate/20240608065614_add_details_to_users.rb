2501447218f8b3f14bb2ce591ad124930617af82
Hirocky23 <key.hiroki23@gmail.com>
2024-06-11 18:56:54 +0900

    修正

--- db/migrate/20240608065614_add_details_to_users.rb
+++ db/migrate/20240608065614_add_details_to_users.rb
@@ -1,9 +1,11 @@
 class AddDetailsToUsers < ActiveRecord::Migration[7.0]
   def change
-    add_column :users, :last_name, :string, null: false
-    add_column :users, :first_name, :string, null: false
-    add_column :users, :last_name_kana, :string, null: false
-    add_column :users, :first_name_kana, :string, null: false
-    add_column :users, :birth_date, :date, null: false
+    add_column :users, :last_name, :string, null: false unless column_exists?(:users, :last_name)
+    add_column :users, :first_name, :string, null: false unless column_exists?(:users, :first_name)
+    add_column :users, :last_name_kana, :string, null: false unless column_exists?(:users, :last_name_kana)
+    add_column :users, :first_name_kana, :string, null: false unless column_exists?(:users, :first_name_kana)
+    add_column :users, :birth_date, :date unless column_exists?(:users, :birth_date)
+    
+    change_column_null :users, :birth_date, false, '1970-01-01' if column_exists?(:users, :birth_date)
   end
 end
