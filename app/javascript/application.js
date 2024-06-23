c6b3f8f7d76eed051d667f695a780e49cc5112d9
Hirocky23 <key.hiroki23@gmail.com>
2024-06-23 23:15:05 +0900

    修正

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,10 +1,5 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 
-//= require rails-ujs
-//= require activestorage
-//= require turbolinks
-//= require_tree .
-
 import "@hotwired/turbo-rails"
 import Rails from "@rails/ujs"
 import * as ActiveStorage from "@rails/activestorage"
