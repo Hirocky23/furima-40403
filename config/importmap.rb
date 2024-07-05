6165d31fd1248bafb8b5d58c7abb1bb30c46eb37
Hirocky23 <key.hiroki23@gmail.com>
2024-07-05 14:14:09 +0900

    修正

--- config/importmap.rb
+++ config/importmap.rb
@@ -7,4 +7,4 @@ pin "@rails/ujs", to: "@rails--ujs.js" # @7.1.3
 pin "@rails/activestorage", to: "https://ga.jspm.io/npm:@rails/activestorage@7.0.4/app/assets/javascripts/activestorage.esm.js", preload: true
 pin "item_price", to: "item_price.js", preload: true
 pin "application", to: "application.js", preload: true
-
+pin "card", to: "card.js"
